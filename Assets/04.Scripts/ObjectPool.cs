73f9832111d701ede9e79c0f0ee017726e189421
akffoddl5 <akffoddl5@naver.com>
2023-10-06 17:41:23 +0900

    asset

---
+++ Assets/04.Scripts/ObjectPool.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    public static ObjectPool instance;
+
+	public GameObject prefab_bullet;
+	public GameObject prefab_explode;
+	
+
+	private void Awake()
+	{
+		if (instance == null)
+		{
+			instance = this;
+			DontDestroyOnLoad(gameObject);
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+	}
+
+	
+
+
+}
--- Assets/04.Scripts/Player_Bullet.cs
+++ Assets/04.Scripts/Player_Bullet.cs
@@ -6,11 +6,11 @@ public class Player_Bullet : MonoBehaviour
 {
 
     public Vector3 move_dir;
-    public float move_speed = 5f;
+    public float move_speed = 25f;
     
     void Update()
     {
         //transform.position = transform.position + move_dir * move_speed * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position, move_dir, 0.1f);
+        transform.position = Vector3.MoveTowards(transform.position, move_dir, 0.9f);
 	}
 }
